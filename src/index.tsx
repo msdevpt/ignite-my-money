8aabd66767079db46f6e9fa28473f31f5aa21884
M.Silva <msilva.kb@outlook.com>
2022-10-08 19:34:54 +0100

    feat: add miragejs server

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'rent',
+          category: 'house',
+          value: -400,
+          date: '2022/10/01'
+        }
+      ]
+    })
+  },
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
