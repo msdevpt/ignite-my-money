c05b3dc5368f541171e29d1ec641e73574dad9e2
M.Silva <msilva.kb@outlook.com>
2022-10-08 21:48:40 +0100

    style: radio buttons colors

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -43,6 +43,7 @@ export function NewTransactionModal({
             type='button'
             onClick={() => {setType('deposit'); }}
             isActive = {type === 'deposit'}
+            activeColor = 'green'
           >
             <img src={incomeImg} alt="Income" />
             <span>Income</span>
@@ -52,6 +53,7 @@ export function NewTransactionModal({
             type='button'
             onClick={() => {setType('withdraw'); }}
             isActive = {type === 'withdraw'}
+            activeColor = 'red'
           >
             <img src={outcomeImg} alt="Outcome" />
             <span>Outcome</span>
