26415784ba5dafd0fdcbc39f7e0bb0286beec775
M.Silva <msilva.kb@outlook.com>
2022-10-08 20:53:55 +0100

    style: modal new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
-import Modal from 'react-modal'
+import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import { Container } from './style';
 
 interface NewTransactionModalProps{
   isOpen: boolean;
@@ -12,10 +14,30 @@ export function NewTransactionModal({
   
   return(
     <Modal 
-          isOpen={isOpen}
-          onRequestClose={onRequestClose}
-        >
-          <h2>New transaction</h2>
-        </Modal>
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button 
+        type='button' 
+        onClick={onRequestClose}
+        className='react-modal-close'
+      >
+        <img src={closeImg} alt="Close new transaction" />
+      </button>
+
+      <Container>
+        <h2>New transaction</h2>
+
+        <input type="text" placeholder='Description' />
+        <input type="number" placeholder='Value' />
+        <input type="text" placeholder='Category' />
+
+        <button type="submit">Save</button>
+
+      </Container>
+      
+    </Modal>
   )
 }
