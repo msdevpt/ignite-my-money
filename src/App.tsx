4e84b190e270f9d780b1929c7c1d6775c9f94d38
M.Silva <msilva.kb@outlook.com>
2022-10-08 20:36:11 +0100

    feat: create component for new transaction

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal'
 
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 Modal.setAppElement('root');
@@ -24,12 +25,10 @@ export function App() {
 
       <Dashboard />
       
-      <Modal 
-          isOpen={isModalTransactionOpen}
-          onRequestClose={handleCloseNewModalTransaction}
-        >
-          <h2>New transaction</h2>
-        </Modal>
+      <NewTransactionModal 
+        isOpen={isModalTransactionOpen} 
+        onRequestClose={handleCloseNewModalTransaction} 
+      />
 
       <GlobalStyle />
     </>
