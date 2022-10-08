de6b1e1f96c7ed66186fa842e00a73ec5e4c64ab
M.Silva <msilva.kb@outlook.com>
2022-10-08 21:42:24 +0100

    feat: create radio button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container, TansactionTypeContainer } from './style';
+import { Container, TansactionTypeContainer, RadioBox } from './style';
+import { useState } from 'react';
 
 interface NewTransactionModalProps{
   isOpen: boolean;
@@ -14,6 +15,8 @@ export function NewTransactionModal({
   onRequestClose
 }:NewTransactionModalProps){
   
+  const [type, setType] = useState('deposit');
+
   return(
     <Modal 
       isOpen={isOpen}
@@ -36,19 +39,23 @@ export function NewTransactionModal({
         <input type="number" placeholder='Value' />
 
         <TansactionTypeContainer>
-          <button
+          <RadioBox
             type='button'
+            onClick={() => {setType('deposit'); }}
+            isActive = {type === 'deposit'}
           >
             <img src={incomeImg} alt="Income" />
             <span>Income</span>
-          </button>
+          </RadioBox>
 
-          <button
+          <RadioBox
             type='button'
+            onClick={() => {setType('withdraw'); }}
+            isActive = {type === 'withdraw'}
           >
             <img src={outcomeImg} alt="Outcome" />
             <span>Outcome</span>
-          </button>
+          </RadioBox>
         </TansactionTypeContainer>
 
         <input type="text" placeholder='Category' />
