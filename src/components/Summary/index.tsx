2cf9dac23675bc986347f39114cc6eeb4d1f2006
M.Silva <msilva.kb@outlook.com>
2022-10-10 21:53:40 +0100

    feat: calc summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -10,7 +10,21 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  console.log(transactions);
+  const summary = transactions.reduce((acc, transaction) => {
+    
+    if (transaction.type === 'deposit'){
+      acc.deposits += transaction.amount;
+    } else {      
+      acc.withdraw += transaction.amount;
+    }
+    acc.total += transaction.amount;
+    
+    return acc;
+  }, {
+    deposits: 0,
+    withdraw: 0,
+    total: 0
+  });
 
   return (
     <Container>
@@ -19,7 +33,10 @@ export function Summary() {
           <p>Income</p>
           <img src={incomeImg} alt="Income" />
         </header>
-        <strong>$1000.00</strong>
+        <strong>{new Intl.NumberFormat('pt-PT', {
+                  style: 'currency',
+                  currency: 'EUR'
+                }).format(summary.deposits)}</strong>
       </div>
       
       <div>
@@ -27,7 +44,10 @@ export function Summary() {
           <p>Outcome</p>
           <img src={outcomeImg} alt="Outcome" />
         </header>
-        <strong>-$500.00</strong>
+        <strong>{new Intl.NumberFormat('pt-PT', {
+                  style: 'currency',
+                  currency: 'EUR'
+                }).format(summary.withdraw)}</strong>
       </div>
 
       <div className='highlight-background'>
@@ -35,7 +55,10 @@ export function Summary() {
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>$500.00</strong>
+        <strong>{new Intl.NumberFormat('pt-PT', {
+                  style: 'currency',
+                  currency: 'EUR'
+                }).format(summary.total)}</strong>
       </div>
     </Container>
   )
